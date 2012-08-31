4eca45659ee10fd198d9b6a4f8fc69c517565eae
hackling <hackling@gmail.com>
2012-08-31 11:55:47 +1000

    Added Google Web Address creation, and JSON decoding

--- googleAPI.rb
+++ googleAPI.rb
@@ -1,3 +1,7 @@
+require "rubygems"
+require "active_support"
+require 'open-uri'
+require 'uri'
 
 
 class GoogleAPI
@@ -5,14 +9,27 @@ class GoogleAPI
   def initialize
     @search_results = Array.new
     @query = ""
+    @url = ""
   end
   
   def add_query(passed_query)
     @query = passed_query
+    @url = 
+    @url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=#{URI.escape(@query.gsub!(" ", "+"))}" 
+    
   end
   
   def return_query
     puts @query
+  end
+  
+  def return_google_address
+    puts @url
+  end
+  
+  def return_search_results
+  result = ActiveSupport::JSON.decode(open(@url).read)
+  puts result
   end
   
     
