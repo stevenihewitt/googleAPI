e8c3f2fa014c684ed1d8052e13c574091fdb1203
hackling <hackling@gmail.com>
2012-08-31 12:13:44 +1000

    API current outputs an Array of Arrays with search data

--- googleAPI.rb
+++ googleAPI.rb
@@ -10,8 +10,10 @@ class GoogleAPI
     @search_results = Array.new
     @query = ""
     @url = ""
+
   end
   
+  #adds the query and creates google url
   def add_query(passed_query)
     @query = passed_query
     @url = 
@@ -19,20 +21,37 @@ class GoogleAPI
     
   end
   
+  #Displays the Query
   def return_query
     puts @query
   end
   
+  #Displays the google search url
   def return_google_address
     puts @url
   end
   
+  #Will return and array or arrays - [title, url, snippet]
   def return_search_results
-  result = ActiveSupport::JSON.decode(open(@url).read)
-  puts result
+    
+    #ecode results
+    result = ActiveSupport::JSON.decode(open(@url).read)["responseData"]["results"]
+  
+  #for each result, add to an array to an array
+    result.each do |page|
+      @search_results << [ page['title'], page['url'], page['content'] ]
+    
+      end
+    
+    #this just displays the data to terminal
+    @search_results.each  do |x| 
+       puts "---Search Result---"+ "-"*30
+       puts x    
+      end
+      puts "-"*50
   end
   
-    
+  #This function is purely for testing purposes  
   def output_test_results
     
     @search_results << ["Webpage 1", "http://www.webpage1.com", "Snippet about Webpage 1"]
--- usecase.rb
+++ usecase.rb
@@ -2,9 +2,10 @@ require "./googleAPI"
 
 test = GoogleAPI.new
 
-test.add_query("TestQuery")
+test.add_query("Test Query Test")
 test.return_query
+test.return_google_address
+#testArray = test.output_test_results
+#puts testArray
 
-testArray = test.output_test_results
-
-puts testArray
+test.return_search_results
