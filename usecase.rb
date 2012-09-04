9d48969590ee97fac6d951852b2b103e422004d3
hackling <hackling@gmail.com>
2012-09-04 12:29:03 +1000

    Fixed Broken Code

--- usecase.rb
+++ usecase.rb
@@ -2,7 +2,7 @@ require "./googleAPI"
 
 test = GoogleAPI.new
 
-test.add_query("Pokemon") #add query
+test.add_query("Parenting 101") #add query
 test.return_query #display query on console
 test.return_google_address #display google url on console
 
